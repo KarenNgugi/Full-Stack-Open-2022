937a4baf649d1f2a37ce9fcf16927d60432acda8
KarenNgugi <karenwngugi12@gmail.com>
2022-09-15 20:22:37 +0300

    made corrections

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,46 +1,46 @@
 const Header = (props) => {
   return (
-    <h1>{props.course}</h1>
+    <h1>{props.name}</h1>
   )
 }
 
 const Content = (props) => {
   return (
-    <div>
-      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
-    </div>
+    props.parts.map(p => <div><p>{p.name} {p.exercises}</p></div>)
   )
 }
 
 const Total = (props) => {
+  
   return (
     <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  const { name, parts } = course;
 
   return (
     <div>
-      <h2>Exercises 1.3-1.5 Refactored React-style</h2>
-      <Header course={course} />
+      <Header name={name} />
       <Content parts={parts} />
       <Total parts={parts} />
     </div>
